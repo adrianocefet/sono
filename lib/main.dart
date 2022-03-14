f16ece273d79d4db41795ea48e46f64f178206b1
reneripardo <reneripardo@gmail.com>
2022-03-13 23:37:48 -0300

    create widgetstateless.

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,23 @@ import 'package:flutter/material.dart';
 // metodo principal do flutter
 main() {
   // metodo para o app funcionar, desenhar na tela 
-  runApp(Container(                                   
-    child: Center( 
-      child: Text( 
-        'TESTE', 
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
-      )
-    ),
-  ));
+  runApp(AppMyWidget());
+}
+
+
+//Widget global (sem estado, imutavel)
+class AppMyWidget extends StatelessWidget{
+  const AppMyWidget({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(                                   
+      child: Center( 
+        child: Text( 
+          'TESTE', 
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
+      ),
+    );
+  }
 }
