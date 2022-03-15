272cee22232e1090c22c5cdb8b7876a6c18438ee
reneripardo <reneripardo@gmail.com>
2022-03-15 00:38:43 -0300

    aula widgetstateless.

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,24 @@ import 'package:flutter/material.dart';
 // metodo principal do flutter
 main() {
   // metodo para o app funcionar, desenhar na tela 
-  runApp(AppMyWidget());
+  runApp(AppMyWidget(
+    title: 'test sono',
+  ));
 }
 
 
 //Widget global (sem estado, imutavel)
-class AppMyWidget extends StatelessWidget{
-  const AppMyWidget({ Key? key }) : super(key: key);
+class AppMyWidget extends StatelessWidget{ 
+  final String title;
+
+  const AppMyWidget({Key? key, this.title="emply"}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(                                   
       child: Center( 
         child: Text( 
-          'TESTE', 
+          title, 
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 50.0),
         ),
