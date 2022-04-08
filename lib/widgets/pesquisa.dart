51edd4c4f8551e31db5e8d3c2b49a12f68651b96
Mateus Machado Costa <75508372+MateusMaccos@users.noreply.github.com>
2022-04-07 22:07:12 -0300

    fix: Busca implementada tab_paciente
    Já é possível clicar e ser direcionado ao perfil do paciente buscado.

--- lib/widgets/pesquisa.dart
+++ lib/widgets/pesquisa.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:sono/utils/models/user_model.dart';
 import 'package:sono/utils/services/firebase.dart';
 
 import 'foto_de_perfil.dart';
@@ -36,73 +38,84 @@ class BarraDePesquisa extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection(identificador).where('hospital',isEqualTo: hospital).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if(snapshot.hasError){
-            return Text('Erro: ${snapshot.error}');
-          }
-          if(!snapshot.hasData){
-            return Text('Erro: sem informação');
-          }else{
-            if(snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())
-            ).isEmpty){
-              return Center(child: Text("Nenhum ${identificador} encontrado"),);
-            }else{
-              return ListView(
-                children: [
-                  ...snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())).map((QueryDocumentSnapshot<Object?> data){
-                    final String title = data.get("Nome");
-                    return ListTile(
-                          onTap: (){
-                            print(data["Nome"]);
-                          },
-                          trailing:Icon(Icons.tag_faces_rounded, size: 40,),
-                          title: Text(title),
-                    );
-                  }
-                  )],
-              );
-            }}
-
-          
-
-        }
-      );
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, model) {
+        return StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection(identificador).where('hospital',isEqualTo: hospital).snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if(snapshot.hasError){
+                return Text('Erro: ${snapshot.error}');
+              }
+              if(snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())
+              ).isEmpty){
+                return Center(child: Text("Nenhuma Resposta encontrada"),);
+              }else{
+                return GridView(
+                            padding: EdgeInsets.only(top:15),
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              childAspectRatio: 1,
+                            ),
+                            scrollDirection: Axis.vertical,
+                            children: [
+                            ...snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())).map((QueryDocumentSnapshot<Object?> data){
+                              return FotoDePerfil.paciente(
+                                data['Foto'] ?? model.semimagem,
+                                data['Nome'] ?? 'sem nome',
+                                data.id,
+                              );
+                            }).toList()]
+                            );
+              }}
+        );}
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context){
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection(identificador).where('Hospital',isEqualTo: hospital).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if(snapshot.hasError){
-            return Text('Error: ${snapshot.error}');
-          }
-
-          switch(snapshot.connectionState){
-              case ConnectionState.waiting:
-                return Center(child: CircularProgressIndicator());
-              default:
-                if(snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())
-                ).isEmpty){
-                  return Center(child: Text("Nenhuma Resposta encontrada"),);
-                }else{
-                   return ListView(
-                      children: [
-                        ...snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())).map((QueryDocumentSnapshot<Object?> data){
-                          final String title = data.get("Nome");
-                          return ListTile(
-                                onTap: (){},
-                                trailing: Icon(Icons.tag_faces_rounded, size: 40,),
-                                title: Text(title),
-                          );
-                        }
-                        )],
-                    );
-                }
-          }
-        },
-      );
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, model) {
+        return StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection(identificador).where('Hospital',isEqualTo: hospital).snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if(snapshot.hasError){
+              return Text('Error: ${snapshot.error}');
+            }
+    
+            switch(snapshot.connectionState){
+                case ConnectionState.waiting:
+                  return Center(child: CircularProgressIndicator());
+                default:
+                  if(snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())
+                  ).isEmpty){
+                    return Center(child: Text("Nenhuma Resposta encontrada"),);
+                  }else{
+                     return GridView(
+                            padding: EdgeInsets.only(top:15),
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              childAspectRatio: 1,
+                            ),
+                            scrollDirection: Axis.vertical,
+                            children: [
+                            ...snapshot.data!.docs.where((QueryDocumentSnapshot<Object?> element) => element['Nome'].toString().toLowerCase().contains(query.toLowerCase())).map((QueryDocumentSnapshot<Object?> data){
+                              return FotoDePerfil.paciente(
+                                data['Foto'] ?? model.semimagem,
+                                data['Nome'] ?? 'sem nome',
+                                data.id,
+                              );
+                            }).toList()]
+                            );
+                  }
+            }
+          },
+        );}
+    );
   }
 }
